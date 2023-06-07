b6bd8adaf2841c2b4628aadf0a53196ef934ceb0
Geonho Shin <geshin@c10r1s8.42seoul.kr>
2023-06-07 17:30:25 +0900

    Update fdf 23.06.07

--- FdF/test.c
+++ FdF/test.c
@@ -3,6 +3,37 @@
 #define WINDOW_WIDTH 1920
 #define WINDOW_HEIGHT 1080
 
+enum EVENT {
+	ON_KEYDOWN = 2,
+	ON_KEYUP = 3,
+	ON_MOUSEDOWN = 4,
+	ON_MOUSEUP = 5,
+	ON_MOUSEMOVE = 6,
+	ON_EXPOSE = 12,
+	ON_DESTROY = 17
+};
+
+enum KEYCODE {
+	KEY_ESCAPE = 53,
+
+	KEY_W = 13,
+	KEY_A = 0,
+	KEY_S = 1,
+	KEY_D = 2
+};
+
+typedef struct s_vars {
+	void *mlx;
+	void *window;
+}	t_vars;
+
+int close(int keycode, t_vars *vars) {
+	if (keycode == KEY_ESCAPE) {
+		mlx_destroy_window(vars->mlx, vars->window);
+	}
+	return 0;
+}
+
 typedef struct s_data {
 	void	*img;
 	char	*addr;
@@ -18,13 +49,13 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color) {
 }
 
 int main(void) {
-	void	*mlx;
-	void	*mlx_window;
+	t_vars vars;
 	t_data	img;
 
-	mlx = mlx_init();
-	mlx_window = mlx_new_window(mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "Hello World!");
-	img.img = mlx_new_image(mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
+	vars.mlx = mlx_init();
+	vars.window = mlx_new_window(vars.mlx, WINDOW_WIDTH, WINDOW_HEIGHT, "Hello World!");
+	mlx_hook(vars.window, ON_KEYDOWN, 1L<<0, close, &vars);
+	img.img = mlx_new_image(vars.mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
 									&img.endian);
 	for (int x = 400; x < 1000; x++) {
@@ -32,6 +63,6 @@ int main(void) {
 			my_mlx_pixel_put(&img, x, y, 0x00ff0000);
 		}
 	}
-	mlx_put_image_to_window(mlx, mlx_window, img.img, 0, 0);
-	mlx_loop(mlx);
+	mlx_put_image_to_window(vars.mlx, vars.window, img.img, 0, 0);
+	mlx_loop(vars.mlx);
 }
