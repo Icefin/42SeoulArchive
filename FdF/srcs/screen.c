4a95ad1df8288679fd14af4c0de5cf329e3d6596
Geonho Shin <geshin@c7r1s9.42seoul.kr>
2023-06-12 18:25:25 +0900

    Update fdf 23.06.12

--- FdF/srcs/screen.c
+++ FdF/srcs/screen.c
@@ -6,7 +6,7 @@
 /*   By: geshin <geshin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/12 14:55:11 by geshin            #+#    #+#             */
-/*   Updated: 2023/06/12 17:24:09 by geshin           ###   ########.fr       */
+/*   Updated: 2023/06/12 18:22:36 by geshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,11 +22,13 @@ void	update_screen(t_program* program)
 {
 	t_image	image;
 	
+	
 	image.img = mlx_new_image(program->mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
 	image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, &image.line_length,
 									&image.endian);
-	//get screen coordinates
-	//draw lines
+	//get screen coordinates from world coordinates
+	
+	//draw lines with VERT_OFFSET
 	for (int x = 400; x < 1000; x++) {
 		for (int y = 300; y < 600; y++) {
 			my_mlx_pixel_put(&image, x, y, 0x00ffffff);
