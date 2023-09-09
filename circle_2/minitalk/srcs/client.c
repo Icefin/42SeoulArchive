2e9eefbde3329702190b5ed61201a52d40a46a38
icefin <wjrchorh135@gmail.com>
2023-09-09 19:03:26 +0900

    Update

--- circle_2/minitalk/srcs/client.c
+++ circle_2/minitalk/srcs/client.c
@@ -3,10 +3,76 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: geshin <geshin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: singeonho <singeonho@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 18:38:24 by geshin            #+#    #+#             */
-/*   Updated: 2023/06/20 18:38:25 by geshin           ###   ########.fr       */
+/*   Updated: 2023/09/09 18:50:33 by singeonho        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
+#include "libft.h"
+
+void	post(int pid, int idx)
+{
+	if (idx == 1)
+		kill(pid, SIGUSR1);
+	else if (idx == 0)
+		kill(pid, SIGUSR2);
+	usleep(100);
+}
+
+void	conv_and_post(int pid, int c, int num)
+{
+	if (!c)
+	{
+		while (num < 8)
+		{
+			post(pid, 0);
+			num++;
+		}
+		return ;
+	}
+	else
+	{
+		conv_and_post(pid, c / 2, ++num);
+		post(pid, c % 2);
+	}
+}
+
+void	post_office(int pid, char *str)
+{
+	int		c;
+	int		num;
+	int		idx;
+	int		last;
+
+	c = 0;
+	idx = -1;
+	last = 7;
+	while (str[++idx])
+	{
+		num = 0;
+		c = (int)str[idx];
+		conv_and_post(pid, c, num);
+	}
+	post(pid, 0);
+	while (last--)
+		post(pid, 1);
+}
+
+int		main(int argc, char *argv[])
+{
+	if (argc != 3)
+		return (0);
+	else
+	{
+		write(1, "Client PID : ", 13);
+		write(1, argv[1], ft_strlen(argv[1]));
+		write(1, "\nMassage : ", 11);
+		write(1, argv[2], ft_strlen(argv[2]));
+		write(1, "\n", 1);
+		post_office(ft_atoi(argv[1]), argv[2]);
+	}
+	return (0);
+}
--- circle_2/minitalk/srcs/server.c
+++ circle_2/minitalk/srcs/server.c
@@ -3,10 +3,91 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: geshin <geshin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: singeonho <singeonho@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 18:38:32 by geshin            #+#    #+#             */
-/*   Updated: 2023/06/20 18:38:33 by geshin           ###   ########.fr       */
+/*   Updated: 2023/09/09 18:53:22 by singeonho        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
+#include <unistd.h>
+
+void	*reset(void *b, size_t len)
+{
+	unsigned char	*temp;
+	unsigned long	i;
+
+	temp = (unsigned char*)b;
+	i = -1;
+	while (++i < len)
+		temp[i] &= 0;
+	return (temp);
+}
+
+void	handler(int signo, siginfo_t *info, void *context)
+{
+	static unsigned char	buf[100];
+	static int				idx;
+	static int				count;
+
+	(void)info;
+	(void)context;
+	if (--count == -1)
+	{
+		count = 7;
+		idx++;
+	}
+	buf[idx] &= ~128;
+	if (signo == SIGUSR1)
+		buf[idx] |= (1 << count);
+	else if (signo == SIGUSR2)
+		buf[idx] &= ~(1 << count);
+	if (buf[idx] == 127 || idx == 99)
+	{
+		write(1, buf, idx + 1);
+		if (buf[idx] == 127)
+			write(1, "\n", 1);
+		reset(buf, 100);
+		idx = 0;
+	}
+}
+
+int		display_pid()
+{
+	char *pid;
+
+	if (!(pid = ft_itoa(getpid())))
+		return (0);
+	write(1, "My PID is ", 10);
+	write(1, pid, ft_strlen(pid));
+	write(1, "\n", 1);
+	free(pid);
+	return (1);
+}
+
+int		main(void)
+{
+	struct sigaction	act1;
+
+	act1.sa_sigaction = handler;
+	act1.sa_flags = SA_SIGINFO;
+	if (!(display_pid()))
+	{
+		write(1, "PID malloc error", 16);
+		exit(1);
+	}
+	if (sigaction(SIGUSR1, &act1, NULL) != 0)
+	{
+		write(1, "Sigaction Error", 15);
+		exit(1);
+	}
+	if (sigaction(SIGUSR2, &act1, NULL) != 0)
+	{
+		write(1, "Sigaction Error", 15);
+		exit(1);
+	}
+	while (1)
+		;
+	return (0);
+}
