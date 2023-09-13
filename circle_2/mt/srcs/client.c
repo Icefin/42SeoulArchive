6757eee1a3099a9820f4356354bd4cc95258ce3e
icefin <wjrchorh135@gmail.com>
2023-09-13 17:56:00 +0900

    Update minitalk 23.09.13

--- circle_2/mt/srcs/client.c
+++ circle_2/mt/srcs/client.c
@@ -3,23 +3,42 @@
 /*                                                        :::      ::::::::   */
 /*   client.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: geshin <geshin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: singeonho <singeonho@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 18:04:40 by geshin            #+#    #+#             */
-/*   Updated: 2023/09/11 19:02:55 by geshin           ###   ########.fr       */
+/*   Updated: 2023/09/13 17:53:54 by singeonho        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <signal.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #define TRUE	1
 #define FALSE	0
 
-static void send()
+static void send_signal(int pid, int signo)
 {
-	
+	if (signo == SIGUSR1)
+		kill(pid, SIGUSR1);
+	else if (signo == SIGUSR2)
+		kill(pid, SIGUSR2);
+	else
+		printf("Invalid Signo Input");
+}
+
+static void process_character_bits(int pid, int utf8)
+{
+
+}
+
+static void send(int pid, char *msg)
+{
+	int	i;
+
+	i = -1;
+	while (msg[++i] != '\0')
+		process_character_bits(pid, msg[i]);
 }
 
 static int is_valid_arguments(int argc, char **argv)
@@ -33,9 +52,8 @@ int main(int argc, char **argv)
 {
 	if (is_valid_arguments(argc, argv) == FALSE)
 		return (1);
-	
+
 	int server_pid = atoi(argv[1]);
-	kill(server_pid, SIGUSR1);
-	kill(server_pid, SIGUSR2);
-	return 0;
+	send(server_pid, argv[2]);
+	return (0);
 }
--- circle_2/mt/srcs/server.c
+++ circle_2/mt/srcs/server.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: geshin <geshin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: singeonho <singeonho@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 18:04:38 by geshin            #+#    #+#             */
-/*   Updated: 2023/09/11 19:04:41 by geshin           ###   ########.fr       */
+/*   Updated: 2023/09/13 17:55:39 by singeonho        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,25 @@
 #define TRUE	1
 #define FALSE	0
 
-void	receive(int signo, siginfo_t *info, void *context)
+static void	receive(int signo, siginfo_t *info, void *context)
 {
-	printf("Receive Some Signal! : %d\n", signo);
+	if (signo == SIGUSR1)
+	{
+
+	}
+	else if (signo == SIGUSR2)
+	{
+
+	}
+	else
+		printf("Undefined Signal Accepted\n");
 }
 
 int main()
 {
 	int	pid = getpid();
 	printf("Server's PID :  %d\n", pid);
-	
+
 	struct sigaction receiver;
 
 	receiver.__sigaction_u.__sa_sigaction = receive;
