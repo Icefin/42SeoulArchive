872e21cc9da118aee353e3d7d6e174758859dddb
GeonHo Shin <wjrchorh135@gmail.com>
2023-10-23 18:33:45 +0900

    Update ms 23.10.23

--- circle_3/minishell/mandatory/srcs/minishell.c
+++ circle_3/minishell/mandatory/srcs/minishell.c
@@ -6,18 +6,26 @@
 /*   By: singeonho <singeonho@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 14:38:00 by singeonho         #+#    #+#             */
-/*   Updated: 2023/10/23 15:36:37 by singeonho        ###   ########.fr       */
+/*   Updated: 2023/10/23 18:30:57 by singeonho        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+
 #define	FALSE	0
 #define	TRUE	1
 
-static void	init_shell();
+static void	init_shell()
+{
+	return ;
+}
 
 int main(int argc, char **argv)
 {
+	char	*input;
+
 	#pragma region SHELL_INIT
 	printf("==========INIT SHELL==========\n");
 	init_shell();
@@ -25,17 +33,19 @@ int main(int argc, char **argv)
 
 	while (TRUE)
 	{
+		input = readline("prompt : ");
+		if (input != NULL)
+			printf("Input : %s\n", input);
+		else
+			continue ;
+		add_history(input);
 		#pragma region PARSING
 		printf("\n\n==========PARSE SHELL==========\n");
 
 
 		#pragma endregion
 
-		#pragma region LEXER
-		printf("\n\n==========LEXER SHELL==========\n");
-
-
-		#pragma endregion
+		free(input);
 
 		#pragma region EXECUTION
 		printf("\n\n==========EXECUTE SHELL==========\n");
