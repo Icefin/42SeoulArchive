5ebcf9460a2d3045e2bb035c964bb0b963c4dd24
icefin <geshin@cx2r1s1.42seoul.kr>
2023-10-30 09:51:34 +0900

    Update philo 23.10.30

--- circle_3/philo/bonus/srcs/philo.c
+++ circle_3/philo/bonus/srcs/philo.c
@@ -3,10 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: singeonho <singeonho@student.42.fr>        +#+  +:+       +#+        */
+/*   By: geshin <geshin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 16:03:14 by singeonho         #+#    #+#             */
-/*   Updated: 2023/10/25 16:03:45 by singeonho        ###   ########.fr       */
+/*   Updated: 2023/10/30 05:38:49 by geshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+//argv[1] : number of philo
+//argv[2] : lifetime of philo
+//argv[3] : eating time
+//argv[4] : sleeping time
+//argv[5] : minimum eating cnt (optional.)
+
+int	main(int argc, char **argv)
+{
+	
+}
