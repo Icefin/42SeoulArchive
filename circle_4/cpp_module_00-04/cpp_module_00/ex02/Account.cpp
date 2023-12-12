a46dd79dafb18f6dfbdeb247be821f0dc762f756
icefin <geshin@c4r4s8.42seoul.kr>
2023-12-12 19:35:48 +0900

    Update cpp 23.12.12

--- circle_4/cpp_module_00-04/cpp_module_00/ex02/Account.cpp
+++ circle_4/cpp_module_00-04/cpp_module_00/ex02/Account.cpp
@@ -6,10 +6,12 @@
 /*   By: geshin <geshin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 18:39:02 by geshin            #+#    #+#             */
-/*   Updated: 2023/12/12 18:46:57 by geshin           ###   ########.fr       */
+/*   Updated: 2023/12/12 19:32:29 by geshin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <time.h>
 #include "Account.hpp"
 
 int Account::getNbAccounts()
@@ -34,28 +36,44 @@ int Account::getNbWithdrawals()
 
 void Account::displayAccountsInfos()
 {
-
+	_displayTimestamp();
+	std::cout << "accounts:" << _nbAccounts << ";";
+	std::cout << "total:" << _totalAmount << ";";
+	std::cout << "deposits:" << _totalNbDeposits << ";";
+	std::cout << "withdrawals:" << _totalNbWithdrawals << '\n';
 }
 
 Account::Account(int initial_deposit)
 {
+	_accountIndex = _nbAccounts;
+	_nbAccounts++;
+
+	_amount = initial_deposit;
+	_totalAmount += _amount;
+	
 	_nbDeposits = initial_deposit;
+	_totalNbDeposits += _nbDeposits;
+	
+	_nbWithdrawals = 0;
 }
 
 Account::~Account()
 {
-	//__noop;
+	_displayTimestamp();
+	
 }
 
 
 void Account::makeDeposit(int deposit)
 {
-
+	_nbDeposits = deposit;
+	_totalNbDeposits += deposit;
 }
 
 bool Account::makeWithdrawal(int withdrawal)
 {
-
+	_nbWithdrawals = withdrawal;
+	_totalNbWithdrawals += withdrawal;
 }
 
 int Account::checkAmount() const
@@ -65,15 +83,32 @@ int Account::checkAmount() const
 
 void Account::displayStatus() const
 {
+	_displayTimestamp();
 	
 }
 
 void Account::_displayTimestamp()
 {
-	
+	time_t timer;
+    struct tm* t;
+    timer = time(NULL);
+    t = localtime(&timer);
+
+	int year = t->tm_year;
+	int mon = t->tm_mon;
+	int date = t->tm_mday;
+	int hour = t->tm_hour;
+	int min = t->tm_min;
+	int sec = t->tm_sec;
+
+	std::cout << '[' << year << mon << date << '_' << hour << min << sec << "] ";
 }
 
 Account::Account()
 {
-	
+	_accountIndex = _nbAccounts;
+	_nbAccounts++;
+	_amount = 0;
+	_nbDeposits = 0;
+	_nbWithdrawals = 0;
 }
