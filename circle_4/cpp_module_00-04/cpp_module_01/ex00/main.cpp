efa541f78e34cdd30f0384617c025d65e0b28288
GeonHo Shin <wjrchorh135@gmail.com>
2023-12-17 17:23:43 +0900

    Update cpp00 23.12.17

--- circle_4/cpp_module_00-04/cpp_module_01/ex00/main.cpp
+++ circle_4/cpp_module_00-04/cpp_module_01/ex00/main.cpp
@@ -6,13 +6,23 @@
 /*   By: singeonho <singeonho@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 18:53:44 by geshin            #+#    #+#             */
-/*   Updated: 2023/12/16 15:44:00 by singeonho        ###   ########.fr       */
+/*   Updated: 2023/12/17 17:23:05 by singeonho        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
+extern Zombie* newZombie(std::string name);
+extern void randomChump(std::string name);
+
 int main(int argc, char** argv)
 {
+	Zombie zombie("Default");
+	zombie.announce();
+	
+	randomChump("RandomChump");
 
+	Zombie* newZomb = newZombie("New");
+	newZomb->announce();
+	delete newZomb;
 }
