9b6b1ed3651cd1fdacecaccb3ca228b764ac1bc5
GeonHo Shin <wjrchorh135@gmail.com>
2024-01-06 02:17:18 +0900

    Update cpp 24.01.06

--- circle_4/cpp_module_00-04/cpp_module_01/ex04/main.cpp
+++ circle_4/cpp_module_00-04/cpp_module_01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: singeonho <singeonho@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/26 20:31:14 by singeonho         #+#    #+#             */
-/*   Updated: 2024/01/06 01:52:27 by singeonho        ###   ########.fr       */
+/*   Updated: 2024/01/06 02:08:03 by singeonho        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,20 @@ int main(int argc, char** argv)
     std::string s1 = argv[2];
     std::string s2 = argv[3];
 
-    std::ifstream inFile(inFileName);
-    if (!inFile.is_open())
+    std::ifstream in;
+    std::ofstream out;
+
+    in.open(inFileName.c_str());
+    if (!in.is_open())
 	{
         std::cout << "Unable to open file: " << inFileName << std::endl;
         return 1;
     }
 
     std::string line;
-    std::ofstream outFile(outFileName);
+    out.open(outFileName.c_str());
 
-    while (std::getline(inFile, line))
+    while (std::getline(in, line))
 	{
         size_t pos = 0;
         while ((pos = line.find(s1, pos)) != std::string::npos)
@@ -46,11 +49,11 @@ int main(int argc, char** argv)
             line.insert(pos, s2);
             pos += s2.length();
         }
-        outFile << line << "\n";
+        out << line << "\n";
     }
 
-    inFile.close();
-    outFile.close();
+    in.close();
+    out.close();
 
     std::cout << "Replacement completed. Output written to '" << outFileName << "'" << std::endl;
     return 0;
