71e4df5a2a0b724e5046c62a2dd9776e5541e51a
GeonHo Shin <wjrchorh135@gmail.com>
2024-01-11 17:00:48 +0900

    Update rt dragon

---
+++ circle_4/cpp_module_00-04/readme.md
@@ -0,0 +1,39 @@
+### Description
+---
+Cpp_module project is for studying cpp98's features.  
+Each module has significant functions or features of cpp.
+
+
+### Dealt features
+---
+#### Cpp_Module_00
+This module is about simple usage of cpp.
+- I/O functions in cpp : cin, cout
+- What does static mean in class
+- Visibility in class : private, protected, public
+
+#### Cpp_Module_01
+This module is about class, reference, function pointer in cpp.
+- Constructor, destructor in cpp
+- How to use new, delete operator, why we need to use them.
+- What's the difference between reference and pointer
+- How to use function pointer in cpp
+
+
+#### Cpp_Module_02
+This module is about operator-overloading by making fixed point number.
+- What is Orhodox Canonical Class Form? What is default constructor, destructor?=
+- What is overloading and how to do that?
+- What's the difference between postfix/prefix increment/decrement operator.
+- Why we need to return reference than default type?
+
+
+#### Cpp_Module_03
+This module is about class inheritance.
+- How to inherit other class in cpp
+- What does virtual keyword mean? why we need to use them?
+- What is a virtual inheritance and why we need to use that?
+
+
+#### Cpp_Module_04
+This module is about polymorphism, abstract class and interface.
--- circle_4/miniRT/bonus/includes/scenes/object.h
+++ circle_4/miniRT/bonus/includes/scenes/object.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   object.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jihwjeon <jihwjeon@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: singeonho <singeonho@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 13:01:45 by singeonho         #+#    #+#             */
-/*   Updated: 2024/01/05 18:50:07 by jihwjeon         ###   ########.fr       */
+/*   Updated: 2024/01/11 16:58:10 by singeonho        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,12 +44,17 @@ typedef struct s_object
 
 	//Cone
 	float			cos_a;
+
+	//Triangle
+	t_vec3			t1;
+	t_vec3			t2;
+	t_vec3			t3;
 }	t_object;
 
 t_object	make_sphere(t_vec3 pos, t_rgb albedo, float radius);
 t_object	make_plane(t_vec3 pos, t_rgb albedo, t_vec3 normal);
 t_object	make_cylinder(t_vec3 pos, t_rgb albedo, float radius, t_vec3 axis, float length);
 t_object	make_cone(t_vec3 pos, t_rgb albedo, float cos_a, t_vec3 axis, float length);
-
+t_object	make_triangle(t_vec3 t1, t_vec3 t2, t_vec3 t3, t_rgb albedo);
 
 #endif
